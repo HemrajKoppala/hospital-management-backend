49fbd2ebe2da0c0674c1245769ba49092f6886bf
Hemraj <hemrajkoppala05@gmail.com>
2026-07-06 12:17:08 +0530

    patient registration added

--- patients/models.py
+++ patients/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.conf import settings
 
-# Create your models here.
+
+class Patient(models.Model):
+
+    user = models.OneToOneField(
+        settings.AUTH_USER_MODEL,
+        on_delete=models.CASCADE
+    )
+
+    name = models.CharField(max_length=100)
+
+    date_of_birth = models.DateField()
+
+    gender = models.CharField(max_length=20)
+
+    phone = models.CharField(max_length=15)
+
+    address = models.TextField()
+
+    blood_group = models.CharField(
+        max_length=5,
+        blank=True
+    )
+
+    created_at = models.DateTimeField(
+        auto_now_add=True
+    )
+
+    def __str__(self):
+        return self.name
